be7240270e018c365eb0bbc9cd3f173e0d260e72
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-12 20:57:47 +0600

    toFixed done

--- hello.js
+++ hello.js
@@ -1,5 +1,56 @@
-console.log(isNaN('marjan'))
-console.log(65);
-console.log(isNaN(65));
+// console.log(isNaN('marjan'))
+// console.log(65);
+// console.log(isNaN(65));
+
+// js Practice 2025
+
+// var apple = 20;
+// var orange = 13;
+// var total = (apple + orange);
+// console.log(total);
+
+// string +
+// let math = 25;
+// let book = 10;
+// console.log(math+book); // 35
+
+// let car = 65;
+// let bus = "50"
+// console.log(car+bus); // output 6550
+
+// var mK = 40;
+// var Kc = "10";
+// console.log(mK-Kc);  // output 30
+
+// var lp = 20;
+// var pP = "10";
+// console.log(lp-pP); // 10 
+
+// var oV = 'hello';
+// var ok = 15;
+// console.log(oV-ok); // NaN
+
+var eo = "45";
+var oe = 10;
+var totals = parseInt(eo);
+// var toto = (totals+oe);
+console.log(totals+oe); // 55
+
+
+var ay = 20;
+var ya = 0;
+console.log(ay/ya); //Infinity
+
+var az = 49.251156156156156;
+console.log(typeof az);
+
+var rp = "55";
+console.log(typeof rp);  // string
+
+var tp = 1.050928456415;
+console.log(tp.toFixed(2));  // 1.05 
+
+
+
 
 
