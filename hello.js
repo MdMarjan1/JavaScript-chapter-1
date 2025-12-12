18a5addfa9189c6742dec0c29a2447ac72c6d5e1
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-12 21:39:55 +0600

    practice done

--- hello.js
+++ hello.js
@@ -4,31 +4,31 @@
 
 // js Practice 2025
 
-// var apple = 20;
-// var orange = 13;
-// var total = (apple + orange);
-// console.log(total);
+var apple = 20;
+var orange = 13;
+var total = (apple + orange);
+console.log(total);
 
 // string +
-// let math = 25;
-// let book = 10;
-// console.log(math+book); // 35
+let math = 25;
+let book = 10;
+console.log(math+book); // 35
 
-// let car = 65;
-// let bus = "50"
-// console.log(car+bus); // output 6550
+let car = 65;
+let bus = "50"
+console.log(car+bus); // output 6550
 
-// var mK = 40;
-// var Kc = "10";
-// console.log(mK-Kc);  // output 30
+var mK = 40;
+var Kc = "10";
+console.log(mK-Kc);  // output 30
 
-// var lp = 20;
-// var pP = "10";
-// console.log(lp-pP); // 10 
+var lp = 20;
+var pP = "10";
+console.log(lp-pP); // 10 
 
-// var oV = 'hello';
-// var ok = 15;
-// console.log(oV-ok); // NaN
+var oV = 'hello';
+var ok = 15;
+console.log(oV-ok); // NaN
 
 var eo = "45";
 var oe = 10;
@@ -48,7 +48,56 @@ var rp = "55";
 console.log(typeof rp);  // string
 
 var tp = 1.050928456415;
-console.log(tp.toFixed(2));  // 1.05 
+console.log(tp.toFixed(2));  // 1.05  tofixed 
+
+
+const aq = 4 - "hlw";
+console.log(aq);    // NaN  Not a Number
+
+let x = '2010';
+let num = Number(x);
+console.log(num);  // Number 2010
+
+let qw = '45.053';
+let to = parseInt(qw);  // 45 answer
+console.log(to);
+
+var sd = '45.8015';
+var sx = parseFloat(sd); // covert to float number
+console.log(sx);
+
+var qa = 4.50665226;
+console.log(qa.toFixed(2)); // 4.51 use toFixed 
+
+var a = isNaN(9);
+console.log(a);  // false
+
+var up = isNaN('sjl');
+console.log(up);  //true
+
+
+var gp = isNaN(45-10);
+console.log(gp); //false
+
+var bp = isNaN(41-"pom");
+console.log(bp);  // true;
+
+var az = false;
+console.log(typeof az);  // boolean
+
+
+var fr = 25;
+var uh = 4;
+var full = (fr%uh);
+console.log(full);   //  1
+
+var kOp = 45;
+kOp+= 10;
+console.log(kOp); // 55
+
+var xz = 100;
+xz = 50;
+console.log(xz);  // 55 
 
 
 
