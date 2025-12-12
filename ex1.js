56fe7c25b2c3f41e15cd829c3817a2ccb28294df
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-12 22:36:27 +0600

    practice done

---
+++ ex1.js
@@ -0,0 +1,27 @@
+//  problem 1
+var paidMoney = 1000;
+var costMoney = 700;
+console.log(paidMoney-costMoney);   // 300
+
+// problem 2
+var math = 75.25;
+var bilology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var allMarks = ((math+bilology+chemistry+physics+bangla)/5);
+console.log(allMarks.toFixed(2));  //71.04
+
+
+// problem 3
+var givenNumber = 5;
+console.log(119%givenNumber); //4
+
+// problem 4
+var a = isNaN("11");
+console.log(a); //false
+
+// problem 5
+var b = isNaN(2-10);
+console.log(b); //false;
