38a10efe3059d3c6f352fd7be4bc85945cc5a49b
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-13 19:39:22 +0600

    practice condition statement

--- day2.js
+++ day2.js
@@ -1,5 +1,76 @@
-var x = null;
-console.log(x); //null  space  open
+// var x = null;
+// console.log(x,"valu is null"); //null  space  open
 
-var y = 
-console.log(y);  //undefined
+// var y = 
+// console.log(y);  //undefined
+
+// let x =5;
+// let y =15;
+// if(x!=y){
+//     console.log('true fuck');  //it define there 2 value are not same
+// }
+
+// problem 1
+// let paidMoney = 550;
+// if(paidMoney > 500){
+//     console.log(" you got Burger & free coke");
+// }
+// else{
+//     console.log(" you can only Coke 30tk")
+// }
+
+
+// problem 2
+// let bmi = 29;
+// if(bmi < 18.5){
+//     console.log("underweight");
+// }
+// else{
+//     if(bmi>=18.5 && bmi<=24.9){
+//         console.log("Normal Weight");
+//     }
+//     else if(bmi>=25 && bmi<=29.9 ){
+//         console.log("overwight");
+//     }
+//     else{
+//         console.log("you are obese");
+//     }
+// }
+
+// var Weight = 30;
+// var height = 1.5;
+
+// var bmi = Weight / (height*height); // bmi calculate
+// if(bmi < 18.5){
+//     console.log("underweight");
+// }
+// else{
+//     if(bmi>=18.5 && bmi<=24.9){
+//         console.log("Normal Weight");
+//     }
+//     else if(bmi>=25 && bmi<=29.9 ){
+//         console.log("overwight");
+//     }
+//     else{
+//         console.log("you are obese");
+//     }
+// }
+
+
+// problem 3
+// var mark = 65;
+// if(mark>=90 && mark<=100){
+//     console.log("A");
+// }else if(mark>=80 && mark<=89){
+//     console.log("B");
+// }else if(mark>=70 && mark<=79){
+//     console.log("C");
+// }else if(mark>=60 && mark<=69){
+//     console.log("D");
+// }else{
+//     console.log("F");
+// }    
+
+
+//problem 4
+
