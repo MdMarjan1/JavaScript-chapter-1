09499d61e984a5f4b83eec2865c3c9d5d553dce9
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-15 12:48:47 +0600

    array practice

--- day3.js
+++ day3.js
@@ -1,3 +1,56 @@
 // array
-let nuMbers = [55, 22, 36, 49, 75, 88, 42, 10]
-console.log(nuMbers);
+// let nuMbers = [55, 22, 36, 49, 75, 88, 42, 10]
+// console.log(nuMbers);
+
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// arr[5] = 11
+
+// console.log(arr); //show all element
+// console.log(arr[2]); // 2
+
+// arr.push(44, 55);  // push add lasr element
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr); // pop remove lasr element
+
+// arr [15] = 20;  // [15 index number]
+// console.log(arr);
+
+// console.log(arr [14]); // undefined
+
+// console.log(arr.length);
+
+let ab1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(ab1.length); // 10
+
+
+let ab2 = ['apple', 'mango', 'banana', 'orange']
+ab2.pop();
+console.log(ab2) // apple mango  banana
+
+
+let ab3 = ['apple', 'banana', 'orange', 'stovery']  
+ab3.push('Cheri','dragon')
+console.log(ab3);  //  'apple', 'banana', 'orange', 'stovery', 'Cheri', 'dragon'
+
+
+let ab4 = ['car', 'boat', 'train', 'bus']
+ab4.shift();
+console.log(ab4); // 'boat', 'train', 'bus'
+
+
+let ab5 = ['car', 'boat', 'train', 'bus']
+ab5.unshift('track');
+console.log(ab5);  //  'track', 'car', 'boat', 'train', 'bus' 
+
+let ab6 = [1, 11, 22, 33, 44, 55]
+ab6[1]=99;
+console.log(ab6); // 1, 99, 22, 33, 44, 55 
+
+
+
+
+
+
+
