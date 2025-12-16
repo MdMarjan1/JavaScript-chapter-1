3dd2b7d0acc1f7de50190f455f1161f7a0dffaf8
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-16 16:48:23 +0600

    while loop practice done

--- ex4_loop.js
+++ ex4_loop.js
@@ -1,59 +1,134 @@
 
 
-// loop
-// Problem 01
-for(let days = 1; days<=60; days++){
-        console.log(days,"I will invest at least 6 hrs every single day for next 60 days!")
-}
+// // loop
+// // Problem 01
+// for(let days = 1; days<=60; days++){
+//         console.log(days,"I will invest at least 6 hrs every single day for next 60 days!")
+// }
+
+
+// // Problem 2
+// //  find odd numbers
+// for(let od = 61; od <= 100; od++){
+//     if(od%2===1){
+//         console.log(od);
+//     }
+// }
+
+// // problem 2
+// //find even numbers
+// for(let ev=78; ev<=98; ev++){
+//     if(ev%2!==1){
+//         console.log(ev);
+//     }
+// }
+
+
+// // problem 3
+// // Display sum of all the odd numbers from 91 to 129.
+// let sum_odd = 0;
+// for(let num1=91; num1<=129; num1++){
+//     if(num1%2!==0){
+//         //console.log(num1);
+//         sum_odd+=num1;
+//     }
+   
+// } console.log(sum_odd);  //2200
+
+
+// // Display sum of all the even numbers from 51 to 85.
+
+// let sum_even = 0;
+// for(let num2=51; num2<=85; num2++){
+//     if(num2%2===0){
+//         sum_even+=num2;
+//     }
+// } console.log(sum_even); // 1156
+
+
+// // provlem 4
+// //Generate a multiplication table for number 9
+// let num=9;
+// for(let i=1; i<=10; i++){
+//     console.log(num,'*',i,'=',num*i);
+// }
+
+
+// // Problem 5
+// for(y=81;y>=65;y--){
+//     console.log(y);
+// }
+
+// While Loop
+
+// Problem 1
+// let v1=1;
+// while(v1<=60){
+//     console.log(v1,'I will invest at least 6 hrs every single day for next 60 days!');
+//     v1++;
+// }
 
 
 // Problem 2
-//  find odd numbers
-for(let od = 61; od <= 100; od++){
-    if(od%2===1){
-        console.log(od);
-    }
-}
+// odd
+// let v2=61;
+// while(v2<=100){
+//     console.log(v2);
+//     v2=v2+2;
+// }
 
-// problem 2
-//find even numbers
-for(let ev=78; ev<=98; ev++){
-    if(ev%2!==1){
-        console.log(ev);
-    }
-}
+// even
+// let v3=78;
+// while(v3<=98){
+//     console.log(v3);
+//     v3=v3+2;
+// }
 
 
 // problem 3
-// Display sum of all the odd numbers from 91 to 129.
-let sum_odd = 0;
-for(let num1=91; num1<=129; num1++){
-    if(num1%2!==0){
-        //console.log(num1);
-        sum_odd+=num1;
-    }
-   
-} console.log(sum_odd);  //2200
+// odd
+// let v4= 81;
+// let sum_odd= 0;
+// while(v4<=131){
+//     if(v4%2===1){
+//         sum_odd+=v4;
+//     }    
+//     v4++
+// } console.log(sum_odd); //2756
+
+// // alternative  odd
+// let v5 = 81;
+// let v6 = 131;
+// let sums = 0;
+// while(v5<=v6){
+//     sums+=v5;
+//     v5+=2;
+//     console.log(sums); 
+// }console.log('total-value',sums); //2756
 
 
-// Display sum of all the even numbers from 51 to 85.
-let sum_even = 0;
-for(let num2=51; num2<=85; num2++){
-    if(num2%2===0){
-        sum_even+=num2;
-    }
-} console.log(sum_even); // 1156
+// // even
+// let v7 = 206;
+// let v8 = 311;
+// let v9_sum = 0;
+// while(v7<=v8){
+//     v9_sum+=v7;
+//     v7+=2;
+// } console.log(v9_sum); //13674
 
 
-// provlem 4
-//Generate a multiplication table for number 9
-let num=9;
-for(let i=1; i<=10; i++){
-    console.log(num,'*',i,'=',num*i);
-}
-
+// problem 4
+// multiplication
+// let k = 5;
+// let p = 1;
+// while(p<=10){
+//     console.log(k,'*',p,'=',k*p);
+//      p++;
+// }
 
 // Problem 5
-for(y=81;y>=65;y--){
-    console.log(y);
+let v8 = 21;
+while(v8>=15){
+    console.log(v8);
+    v8--;
 }
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,6 @@
-console.log(num1);
+let k = 5;
+// let p = 1;
+// while(p<=10){
+//     console.log(k,'*',p,'=',k*p);
+//      p++;
+// }
