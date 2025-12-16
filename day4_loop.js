2fcc81b728a0d358e9943b8c4cecf98240b97184
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-16 12:38:59 +0600

    for loop practice

--- day4_loop.js
+++ day4_loop.js
@@ -1,9 +1,8 @@
-var ab25 = "hello";
-var ab23 = [45, 55, 65, 75, 85, 95]
-var ab24 = [];
-
-
-
-console.log(Array.isArray(ab23));
-console.log(Array.isArray(ab24));
-console.log(Array.isArray(ab25));
+let num = 1;
+while (num<=10){
+ //   console.log(num);
+    if(num%2===0){
+        console.log("even",num);
+    }
+   num++
+}
---
+++ ex4_loop.js
@@ -0,0 +1,25 @@
+// loop
+// Problem 01
+// for(let days = 1; days<=60; days++){
+//         console.log(days,"I will invest at least 6 hrs every single day for next 60 days!")
+// }
+
+
+
+// Problem 2
+//  find odd numbers
+// for(let od = 61; od <= 100; od++){
+//     if(od%2===1){
+//         console.log(od);
+//     }
+// }
+
+// problem 2
+// find even numbers
+// for(let ev=78; ev<=98; ev++){
+//     if(ev%2!==1){
+//         console.log(ev);
+//     }
+// }
+
+// Display sum of all the odd numbers from 91 to 129.
