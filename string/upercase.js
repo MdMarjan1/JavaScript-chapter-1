dd906dfea078923c9d246f572c45f3d6c2713472
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-17 14:10:52 +0600

    lowercase done

--- string/upercase.js
+++ string/upercase.js
@@ -1,21 +1,21 @@
-// let a = 'bangladesh';
-// //console.log(a);
-// console.log(a.toUpperCase());  // BANGLADESH
+let a = 'bangladesh';
+//console.log(a);
+console.log(a.toUpperCase());  // BANGLADESH
 
-// let b = 'Shariatpur';
-// console.log(b.toLowerCase()); // shariatpur
+let b = 'Shariatpur';
+console.log(b.toLowerCase()); // shariatpur
 
 
 
-// let subject = 'Chemistry';
-// let book = 'ChemIsTry';
+let subject = 'Chemistry';
+let book = 'ChemIsTry';
 
-// if(subject.toLowerCase() === book.toLowerCase()){
-//     console.log("pora pari");
-// }
-// else{
-//     console.log("hudai pora")
-// }
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log("pora pari");
+}
+else{
+    console.log("hudai pora")
+}
 
 let v1 = 'water ';
 let v2 = ' water ';
