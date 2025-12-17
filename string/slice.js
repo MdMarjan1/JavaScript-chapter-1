47da3ddceec524b6ea0d42427b8cc44928439a79
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-17 14:52:37 +0600

    slice join concat

---
+++ string/slice.js
@@ -0,0 +1,21 @@
+// let a1 = 'america';
+// console.log(a1.slice(1,4));
+
+// let a2 = "I am a Web pro Developer";
+// console.log(a2.split(' '))
+// console.log(a2.split(''))
+// console.log(a2.split('a'))
+
+let friendStr = 'rahim, horim, abul, jomir, kalam';
+let beg = friendStr.split(',');
+console.log(beg);
+
+let rog = [ 'rahim', ' horim', ' abul', ' jomir', ' kalam' ];
+console.log(rog.join('-'));
+
+var fst_name = 'foysal';
+var lst_name = 'ahemad';
+console.log(fst_name.concat(' ').concat(lst_name));
+
+var th_name = "Mongoliya"
+console.log(th_name.includes("g"));
