d742ef76acda9d100ed78438d3e6c848d9d92f4c
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-17 14:09:24 +0600

    uppercase add

--- string/sreing_length.js
+++ string/sreing_length.js
@@ -1,2 +1,12 @@
 let capital = 'Dhaka';
-console.log(capital.length);
+console.log(capital.length); // 5
+
+
+let place_name= "Rangamati";
+console.log(place_name.length); // length 9
+console.log(place_name[3]);  // g  (index num 3 value: g)
+
+let jayga = "Donia";
+jayga[1]="w";
+console.log(jayga); // string is immutable not changed there value
+
---
+++ string/upercase.js
@@ -0,0 +1,27 @@
+// let a = 'bangladesh';
+// //console.log(a);
+// console.log(a.toUpperCase());  // BANGLADESH
+
+// let b = 'Shariatpur';
+// console.log(b.toLowerCase()); // shariatpur
+
+
+
+// let subject = 'Chemistry';
+// let book = 'ChemIsTry';
+
+// if(subject.toLowerCase() === book.toLowerCase()){
+//     console.log("pora pari");
+// }
+// else{
+//     console.log("hudai pora")
+// }
+
+let v1 = 'water ';
+let v2 = ' water ';
+if(v1.trim() === v2.trim()){
+    console.log('easy');
+}
+else{
+    console.log('hard')
+}
