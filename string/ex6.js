59afdecdec8a7e59c78b886841183cef5d590322
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-19 23:38:07 +0600

    string solved

--- string/ex6.js
+++ string/ex6.js
@@ -1,5 +1,6 @@
-// let a1 = 0;
-// let teXt = 'Banana';
+//  Problem 1
+let a1 = 0;
+let teXt = 'Banana';
 
 // for(let a2=0; a2<teXt.length; a2++){
 //     if(teXt[a2] === 'a'){
@@ -8,37 +9,37 @@
 //     }
 // } console.log(a1);
 
-// for(let a4 of teXt){
-//     if(a4 === 'a'){
-//         a1++;
-//     }
-// }
-// console.log(a1);
+for(let a4 of teXt){
+    if(a4 === 'a'){
+        a1++;
+    }
+}
+console.log(a1);
 
-// // Problem 2
-// let p1 = 0;
-// let Mms = 'bananaAAA'
-// for(let p2 of Mms){
-//     if(p2 === 'a' || p2 === 'A'){
-//         p1++;
-//     }
-// }
-// console.log(p1);
+// Problem 2
+let p1 = 0;
+let Mms = 'bananaAAA'
+for(let p2 of Mms){
+    if(p2 === 'a' || p2 === 'A'){
+        p1++;
+    }
+}
+console.log(p1);
 
-// // Problem 3
-// let v1 = "Hello World";
-// if(v1.includes('a') && v1.includes('e') &&v1.includes('i')&& v1.includes('o') && v1.includes('u')){
-//     console.log("all avaiable")
-// }else{
-//     console.log("No");
-// }
+// Problem 3
+let v1 = "Hello World";
+if(v1.includes('a') && v1.includes('e') &&v1.includes('i')&& v1.includes('o') && v1.includes('u')){
+    console.log("all avaiable")
+}else{
+    console.log("No");
+}
 
-// // Problem 4
-// let x = "Xerox example Box";
-// x = x.replaceAll('x', 'y');
-// x = x.replaceAll('X', 'Y')
+// Problem 4
+let x = "Xerox example Box";
+x = x.replaceAll('x', 'y');
+x = x.replaceAll('X', 'Y')
 
-// console.log(x);
+console.log(x);
 
 
 //  // Problem 5
