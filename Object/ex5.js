6bf14d4a70d5536f23936aa0b8233104c2e7d3e9
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-19 12:10:37 +0600

    Problem 2 solved

--- Object/ex5.js
+++ Object/ex5.js
@@ -1,7 +1,22 @@
 // Object
-let color = {
-    red: "#ff0000",
-    green: "#00ff00",
-    blue: "#0000ff",
-    "golden rod": '#daa520',
-}
+// Problem 1
+// let colors = {
+//     red: "#ff0000",
+//     green: "#00ff00",
+//     blue: "#0000ff",
+//     "golden rod": '#daa520',
+// }
+//     console.log(colors["golden rod"]);
+
+
+// Problem 2
+// const car = {
+//     make: "Toyata",
+//     model: "Corolla",
+//     year: 2020,
+
+// };
+//     car.passenger_capacity = 5;
+//     // car["passenger_capacity"] = 5  // alternative way
+//     console.log(car);
+
