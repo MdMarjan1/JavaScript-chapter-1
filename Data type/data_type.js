22450bc26f2b1d416e89324aaec92dd39ee7002c
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-20 10:43:17 +0600

    practice

---
+++ Data type/data_type.js
@@ -0,0 +1,19 @@
+// Data type
+
+// Primitive
+// 1  4.5  8  2.12  
+//  String  > "this is also text"
+//Boolean > true > false
+
+// undefined
+// null
+
+
+
+
+
+
+// Object
+// 1. array
+// 2. aabject
+// 3. Funtion
