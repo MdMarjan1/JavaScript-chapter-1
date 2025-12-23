5c315da5b9c44face2f460506c134200edf73072
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-23 21:08:20 +0600

    problem solving array looping

---
+++ array/ax7.js
@@ -0,0 +1,9 @@
+let friends = ['raju', 'kalam', 'alom', 'milon', 'jack']
+
+for(let i =0; i<friends.length; i++){
+    console.log(friends[i]);
+}
+
+// for(let friend of friends){
+//     console.log(friend);
+// }
--- ex3_array.js
+++ ex3_array.js
@@ -1,48 +1,59 @@
 
-// problem 1
-let fruits = ['apple', 'mango', 'banana', 'orange', 'dragon']
-console.log(fruits[3]); //orange
-fruits[2]='jambura';
-console.log(fruits);  //'apple', 'mango', 'jambura', 'orange', 'dragon'
+// // problem 1
+// let fruits = ['apple', 'mango', 'banana', 'orange', 'dragon']
+// console.log(fruits[3]); //orange
+// fruits[2]='jambura';
+// console.log(fruits);  //'apple', 'mango', 'jambura', 'orange', 'dragon'
 
 
-// problem 2
+// // problem 2
+
+// // let touristPlace = ['coxsbazar', 'sylhet', 'sajek']
+// // console.log(touristPlace); //'coxsbazar', 'sylhet', 'sajek'
+// // touristPlace.unshift('rangamati')
+// // console.log(touristPlace); // 'rangamati', 'coxsbazar', 'sylhet', 'sajek'
+// // touristPlace.push('sonargaw', 'moynamoti')
+// // console.log(touristPlace);
+// // touristPlace.pop();
+// // console.log(touristPlace);
 
 // let touristPlace = ['coxsbazar', 'sylhet', 'sajek']
-// console.log(touristPlace); //'coxsbazar', 'sylhet', 'sajek'
 // touristPlace.unshift('rangamati')
-// console.log(touristPlace); // 'rangamati', 'coxsbazar', 'sylhet', 'sajek'
 // touristPlace.push('sonargaw', 'moynamoti')
-// console.log(touristPlace);
 // touristPlace.pop();
-// console.log(touristPlace);
-
-let touristPlace = ['coxsbazar', 'sylhet', 'sajek']
-touristPlace.unshift('rangamati')
-touristPlace.push('sonargaw', 'moynamoti')
-touristPlace.pop();
-console.log(touristPlace); //'rangamati', 'coxsbazar', 'sylhet', 'sajek', 'sonargaw'
+// console.log(touristPlace); //'rangamati', 'coxsbazar', 'sylhet', 'sajek', 'sonargaw'
 
 
-//problem 3
-//‘includes’
-let books = ['herry poter', 'sherloks homes', 'learn js', 'p by python']
-console.log(books.includes('learn js')); // true
+// //problem 3
+// //‘includes’
+// let books = ['herry poter', 'sherloks homes', 'learn js', 'p by python']
+// console.log(books.includes('learn js')); // true
 
 
 
-//problem 4
-let ed = [55, 44, 33];
-let bf = '[]';
-let df = { name: "JS" };
-console.log(Array.isArray(ed)); // true
-console.log(Array.isArray(bf)); // false
-console.log(Array.isArray(df)); // false
+// //problem 4
+// let ed = [55, 44, 33];
+// let bf = '[]';
+// let df = { name: "JS" };
+// console.log(Array.isArray(ed)); // true
+// console.log(Array.isArray(bf)); // false
+// console.log(Array.isArray(df)); // false
 
-//problem 5
-let tourist = ['coxsbazar', 'sylhet', 'sajek']
-let aa = [55, 44, 33];
-let cOm=tourist.concat(aa);
-console.log(cOm); // 'coxsbazar', 'sylhet', 'sajek', 55, 44, 33 
+// //problem 5
+// let tourist = ['coxsbazar', 'sylhet', 'sajek']
+// let aa = [55, 44, 33];
+// let cOm=tourist.concat(aa);
+// console.log(cOm); // 'coxsbazar', 'sylhet', 'sajek', 55, 44, 33 
 
-``
+const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+// const xy = colors.reverse();
+// console.log(xy);
+
+let dd = []
+for(let x of colors){
+   // console.log(x);
+    dd.unshift()
+
+}
+console.log(dd);
+ 
