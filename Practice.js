5b78e797c0bc1e72021caa5ebff6fb1893c7b057
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-24 22:24:14 +0600

    boolean practice

---
+++ Practice.js
@@ -0,0 +1,13 @@
+// let username = "admin";
+// let password = "1234";
+// let isLoggedIn = false;
+
+// if (username === "admin" && password === "1235") {
+//     isLoggedIn = true;
+// }
+
+// if (!isLoggedIn) {
+//     console.log("❌ Login failed");
+// } else {
+//     console.log("✅ Login success");
+// }
