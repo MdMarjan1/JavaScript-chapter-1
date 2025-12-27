3900f177bbcae59a9fe6e0323ea5bed2695580f9
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-27 19:09:35 +0600

    practice

---
+++ Function/frist_function.js
@@ -0,0 +1,25 @@
+// function syntx
+
+// function callName(){
+//     var a1 = 20;
+//     var a2 = 10;
+//     console.log(a1+a2);
+// }
+// callName(); // 30
+
+
+// function isName (a,b){
+//     var result = (a+b);
+//     console.log(result);
+// }
+// isName(10, 15); // 25
+
+// let a5 = [1, 2, 3]
+// function sameOfArray(arr){
+//     for(let i=0; i<arguments.length; i++){
+//         arr[i]
+//     }
+//     console.log(arr);
+// }
+// sameOfArray(a5);
+
