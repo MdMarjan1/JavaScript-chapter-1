9359293518c6e50458705f20b1b2b86980c72ff2
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-29 01:06:25 +0600

    practice problem solved

--- Function/exercise.js
+++ Function/exercise.js
@@ -8,16 +8,70 @@
 
 
 // Problem 02
-function doUble (nas){
-    if(nas % 2 === 1){
-        let result = nas * 2
-        return result
-    }
-    else{
-        let result = nas / 2
-        return result
-    }
+// function doUble (nas){
+//     if(nas % 2 === 1){
+//         let result = nas * 2
+//         return result
+//     }
+//     else{
+//         let result = nas / 2
+//         return result
+//     }
+// }
+// let take = 5
+// let opo = doUble(5)
+// console.log(opo);
+
+
+
+// Problem 03
+function makeAvg(number){
+   let sum = 0
+   for(let i= 0; i<number.length; i++){
+        sum+=number[i]
+   }
+    var aa = sum / number.length
+   // console.log(aa)
+    return aa;
 }
-let take = 5
-let opo = doUble(5)
-console.log(opo);
+makeAvg([30,25,12,5,48])
+// ----------------------------------------
+// function makeAvg(number){
+//    let sum = 0
+//    for(num of number){
+//         sum+=num
+//    }
+//     var aa = sum / number.length
+//     console.log(aa)
+//     return aa;
+// }
+// makeAvg([30,25,12,5,48])
+
+
+
+// Problem 04
+
+//  function countZero (str){
+//      let sos = 0
+//      for(nas of str){
+//           if(nas === '1'){
+//           nas++
+//           sos+=nas
+//           }   
+//      }
+//      return sos
+//  }
+// let fonal = countZero('10010010001')
+// console.log(fonal)
+// ---------------------------------------
+function count_zero (str1){
+     let soa = 0
+     for(nou of str1){
+          if(nou === '0'){
+          soa++
+          }
+     }
+     return soa
+}
+let final1 = count_zero('101100010')
+console.log(final1)
