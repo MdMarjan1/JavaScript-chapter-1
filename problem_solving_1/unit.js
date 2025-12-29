585886f9a62461f7fbc0263da0cc3b6ac04bfd79
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-29 21:18:20 +0600

    unit problem solved

---
+++ problem_solving_1/unit.js
@@ -0,0 +1,14 @@
+function kmTomile(km){
+    let mile = km * 0.621371
+    return mile
+}
+ let miles = kmTomile(5)
+ console.log(miles)
+
+
+ function miTokm(mil){
+    let km = mil * 1.60934
+    return km
+}
+ let kilos = miTokm(25)
+ console.log(kilos)
