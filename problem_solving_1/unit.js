e0f6466a84a2c7b2b6b861ab076769bda8ca64b1
mdmarjan-pg <mdmarjan313.it@gmail.com>
2025-12-31 14:08:28 +0600

    frist problem solved

--- problem_solving_1/unit.js
+++ problem_solving_1/unit.js
@@ -1,14 +1,31 @@
-function kmTomile(km){
-    let mile = km * 0.621371
-    return mile
-}
- let miles = kmTomile(5)
- console.log(miles)
+// function kmTomile(km){
+//     let mile = km * 0.621371
+//     return mile
+// }
+//  let miles = kmTomile(5)
+//  console.log(miles)
 
 
- function miTokm(mil){
-    let km = mil * 1.60934
-    return km
+//  function miTokm(mil){
+//     let km = mil * 1.60934
+//     return km
+// }
+//  let kilos = miTokm(25)
+//  console.log(kilos);
+
+
+
+// problem 01
+// c to f  ------f=(9c+(32*5))/5
+
+function celTofah (tem){
+    let a01 = (tem * (9/5))+32
+
+      return a01
 }
- let kilos = miTokm(25)
- console.log(kilos);
+
+let a02 = celTofah(25)
+console.log(a02)
+
+  
+
