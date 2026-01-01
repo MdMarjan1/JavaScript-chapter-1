e29065b03a96b7ba7f2d8445fc636ab88a32382e
mdmarjan-pg <mdmarjan313.it@gmail.com>
2026-01-02 00:25:55 +0600

    practice file

--- problem_solving_1 & 2/unit1.js
+++ problem_solving_1 & 2/unit1.js
@@ -54,8 +54,66 @@
 
 // ----------------------------------------------------
 
-function CheapPhone(price){
+// function CheapPhone(price){
+//     let lo1 = price[0]
+//     for(let lowPh of price){
+//         if(lowPh < lo1){
+//             lo1 = lowPh
+//         }
 
+//     }
+//     return lo1
+
+// }
+
+// let phnPrices = [2000, 10000, 10500, 9000, 100000]
+// let ios = CheapPhone(phnPrices)
+// console.log(ios)
+
+// --------------------------------------------------------
+
+// let phones =[
+//     {name: "Samsung", camera: "25px", price: 500, color:"white"},
+//     {name: "Nokia", camera: "50px", price: 25300, color:"blue"},
+//     {name: "Vivo", camera: "15px", price: 5000, color:"red"},
+//     {name: "xiomi", camera: "65px", price: 45000, color:"Orange"},
+//     {name: "Reami", camera: "36px", price: 35000, color:"grey"},
+// ]
+
+
+// function getCheapPhone(pera){
+//     let min = pera[0];
+//     for(let phn of pera){
+//         if(phn.price < min.price){
+//             min = phn
+//         }
+//     }
+//     return min
+// }
+
+// let chop = getCheapPhone(phones)
+// console.log(chop)
+
+// ---------------------------------------------------------------------
+
+
+let products =[
+    {name: "shampoo", price: 250},
+    {name: "soup", price: 50},
+    {name: "box", price: 570},
+    {name: "potato", price: 150},
+    {name: "bendi", price: 350},
+]
+
+function totalCost(costs){
+    let soms = 0
+    for(let cost of costs){
+        soms += cost.price 
+    }
+    return soms
 }
 
-let phnPrices = [2000, 10000, 10500, 9000, 100000]
+let money = totalCost(products)
+console.log(money)
+
+// ----------------------------------------------------------
