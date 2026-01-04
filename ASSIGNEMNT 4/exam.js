a1a57f1d9baab27fdc5503a5576139c6b82a50ce
mdmarjan-pg <mdmarjan313.it@gmail.com>
2026-01-04 14:21:15 +0600

    problem 2

--- ASSIGNEMNT 4/exam.js
+++ ASSIGNEMNT 4/exam.js
@@ -1,21 +1,5 @@
 // Problem 01
 
-// function cashOut(cashMoney){
-//     let charge = cashMoney * 1.75 / 100
-//     if(typeof charge !== "number" || charge < 0){
-//         return "invalid"
-//     }
-//     else{
-//         return charge
-//     }
-// }
-
-// let cashOutmoney = cashOut("")
-// console.log(cashOutmoney)
-
-// ------------------------------------------------------
-
-// Problem 02
 function cashOut(cashMoney){
     if(typeof cashMoney !== "number" || cashMoney < 0){
         return "invalid"
@@ -24,11 +8,45 @@ function cashOut(cashMoney){
     return charge
 }
      let cashOutmoney = cashOut(2000)
-     console.log(cashOutmoney)
+    //  console.log(cashOutmoney)
 
 //--------------------------------------------------------
 
+// Problem 02
+function validMail(mail){
+     if(typeof mail !== "string"){
+        return "invalid";
+    }
+
+    if(mail.startsWith("@") || mail.startsWith(".") ||  mail.startsWith("-") || mail.startsWith("_") ||  mail.startsWith("+") || mail.includes(" ") || mail.includes("-")){
+        return false;
+    }
+    if(mail.includes("@") && mail.endsWith(".com")){
+        return true;
+    }
+
+    return false;
+
+}
+
+let email = validMail("chat420@gpt.net")
+console.log(email);
+
+// ferdous@gmail.com / true
+// lzihad@gmail.com / true
+// .king@yahoo.com / false
+// Mewo@cat.com / true
+// ["jhankar@hero.com"] / invalid
+// true / invalid
+// programming-hero.com // false
+// chat420@gpt.net //false
+
+
+//---------------------------------------------------------
+
 // Problem 03
+
+
 
 
 
