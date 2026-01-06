59f6ddd21ebbf66b3db4773edd64fb22a0f6371c
mdmarjan-pg <mdmarjan313.it@gmail.com>
2026-01-06 21:04:58 +0600

    use for of loop

--- ASSIGNEMNT 4/exam.js
+++ ASSIGNEMNT 4/exam.js
@@ -1,52 +1,25 @@
-// Problem 01
+// practice
+let products=[
+    {id:1, name:"mi Phone", price: 19000},
+    {id:2, name:"samsung phone", price: 25000},
+    {id:3, name:"vivo for high", price:30000},
+    {id:4, name:"realme phone", price:35000},
+    {id:5, name:"infinix button phone", price:89000},
+    {id:6, name:"apple Iphone", price:78000}
+]
 
-function cashOut(cashMoney){
-    if(typeof cashMoney !== "number" || cashMoney < 0){
-        return "invalid"
+// for(let product of products){
+//     console.log(product)
+// }
+
+function findPhone(products, surch){
+    let matbox = []
+    for(let product of products){
+        if(product.name.toLowerCase().includes(surch)){
+            matbox.push(product)
+        }
     }
-    let charge = cashMoney * 1.75 / 100
-    return charge
+    return matbox
 }
-     let cashOutmoney = cashOut(2000)
-    //  console.log(cashOutmoney)
-
-//--------------------------------------------------------
-
-// Problem 02
-function validMail(mail){
-     if(typeof mail !== "string"){
-        return "invalid";
-    }
-
-    if(mail.startsWith("@") || mail.startsWith(".") ||  mail.startsWith("-") || mail.startsWith("_") ||  mail.startsWith("+") || mail.includes(" ") || mail.includes("-")){
-        return false;
-    }
-    if(mail.includes("@") && mail.endsWith(".com")){
-        return true;
-    }
-
-    return false;
-
-}
-
-let email = validMail("chat420@gpt.net")
-console.log(email);
-
-// ferdous@gmail.com / true
-// lzihad@gmail.com / true
-// .king@yahoo.com / false
-// Mewo@cat.com / true
-// ["jhankar@hero.com"] / invalid
-// true / invalid
-// programming-hero.com // false
-// chat420@gpt.net //false
-
-
-//---------------------------------------------------------
-
-// Problem 03
-
-
-
-
-
+let socislWork = findPhone(products,'phone'.toLowerCase())
+console.log(socislWork)
--- ASSIGNEMNT 4/tempCodeRunnerFile.js
+++ ASSIGNEMNT 4/tempCodeRunnerFile.js
@@ -1,24 +1,3 @@
-
-// function validMail(mail) {
-//     // ১. Check input is string
-//     if (typeof mail !== "string") {
-//         return "invalid";
-//     }
-
-//     // ২. Check email should not start with invalid characters or contain spaces
-//     if (mail.startsWith("@") || mail.startsWith(".") || mail.startsWith("-") || 
-//         mail.startsWith("_") || mail.startsWith("+") || mail.includes(" ")) {
-//         return false;
-//     }
-
-//     // ৩. Email should include "@" in middle and end with ".com"
-//     if (mail.includes("@") && mail.endsWith(".com")) {
-//         return true;
-//     }
-
-//     // ৪. সব other cases invalid
-//     return false;
-// }
-
-// // ✅ Test cases
-// console.log(validMail("programming-hero.com")); // false
+ // if(typeof number !== "number"){
+    //     return "invalid"
+    // }
